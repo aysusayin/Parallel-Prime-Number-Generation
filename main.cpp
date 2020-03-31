179cd5693b3c1587afa4a38a301c4ae26574256e
aysusayin <aysusayin@gmail.com>
2020-03-31 17:18:39 +0300

    some parallel code

--- main.cpp
+++ main.cpp
@@ -6,6 +6,8 @@
 #include <sstream>
 #include <algorithm>
 #include <queue>
+#include <cmath>
+#include <omp.h>
 
 using namespace std;
 
@@ -26,60 +28,138 @@ static int chunkSize;
 static int M;
 static int executionTime;
 
+// Calculates primes between [start, end]
+int primeNumberGenerator(vector<int> &primesIn, vector<int> &primesOut, int start, int end) {
+    int realStart = start;
+    if (start <= 2) {
+        primesOut.push_back(2);
+        realStart = 1;
+    } else if (start % 2 == 0) {
+        realStart -= 1;
+    }
+    int j;
+    int k;
+    int n;
+    int quo, rem;
+    n = realStart;
 
-int primeNumberGenerator(vector<int> prime, int t, int M){
-    int j ;
-    int k ;
-    int n ;
-    int quo,rem ;
-    prime.push_back(2);
-    n = 3 ;
-    for(j = 1; n < M; j++){// P2
-        prime.push_back(n);
+    for (j = 1; n <= end; j++) {// P2
+        if (j != 1) primesOut.push_back(n);
         bool check = true;
-        while(check){//p4
-            n+=2;
-            k = 1;
-            while(true){//p6
-                quo = n / prime.at(k) ;
-                rem = n % prime.at(k) ;
+        while (check) {//p4
+            n += 2;
+            for (k = 1; k < primesIn.size(); k++) {//p6
+                quo = n / primesIn.at(k);
+                rem = n % primesIn.at(k);
                 if (rem == 0) {
                     break;
                 }
-                if (quo <= prime.at(k)){
+                if (quo <= primesIn.at(k)) {
                     check = false;
                     break;
                 }
-                k++;
             }
-        } 
+            if (k >= primesIn.size()) check = false;
+        }
+
     }
-    
-    for(j=0; j < prime.size(); j++){ printf("%d\n", prime.at(j));}
+
+    for (j = 0; j < primesOut.size(); j++) { printf("%d\n", primesOut.at(j)); }
 }
 
-int main(int argc, char *argv[]) { 
-    if(argc < 4){
+// Calculates prime numbers up to end.
+int primeNumberGenerator(vector<int> &primes, int end) {
+    primeNumberGenerator(primes, primes, 1, end);
+}
+
+int main(int argc, char *argv[]) {
+    if (argc < 4) {
         cout << "All arguments are required";
         return 0;
-    }   
+    }
     schedulingMethod = argv[1];
     transform(schedulingMethod.begin(), schedulingMethod.end(), schedulingMethod.begin(), ::toupper);
     chunkSize = stoi(argv[2]);
     M = stoi(argv[3]);
     vector<int> prime;
-    primeNumberGenerator(prime, 1, M);
-    if(schedulingMethod.compare("STATIC")){
+    vector<int> primeOut;
+    int squareRootM = (int) sqrt(M);
+    primeNumberGenerator(prime, squareRootM);
+
+
+    if (schedulingMethod.compare("STATIC")) {
+        cout << schedulingMethod;
+    } else if (schedulingMethod.compare("DYNAMIC")) {
+        cout << schedulingMethod;
+    } else if (schedulingMethod.compare("GUIDED_A")) {
+        cout << schedulingMethod;
+    } else if (schedulingMethod.compare("GUIDED_B")) {
         cout << schedulingMethod;
     }
-    else if (schedulingMethod.compare("DYNAMIC")){
-        cout << schedulingMethod;
+
+    int tid;
+    vector<vector<int>> threads_prime;
+#pragma omp parallel private(tid) shared(threads_prime, M, squareRootM, prime, chunkSize)
+    {
+        tid = omp_get_thread_num();
+        printf("Hello World from thread = %d\n", tid);
+
+        if (tid == 0) {
+            int num_threads = omp_get_num_threads();
+            for (int i = 0; i < num_threads; i++) {
+                vector<int> tmp;
+                threads_prime.push_back(tmp);
+            }
+        }
+
+#pragma omp barrier
+
+        vector<int> primesOut = threads_prime.at(tid);
+        int end = M;
+        int realStart = squareRootM;
+        if (squareRootM <= 2) {
+            primesOut.push_back(2);
+            realStart = 1;
+        } else if (squareRootM % 2 == 0) {
+            realStart -= 1;
+        }
+        int k;
+        int n = realStart;
+
+#pragma omp for private(primesOut, k) schedule(static, chunkSize)
+        for (int j = 1; j <= end; j++) {// P2
+            if (n <= end) {
+                if (j != 1) primesOut.push_back(n);
+                printf("pushed %d from %d\n", n, tid);
+                bool check = true;
+                while (check) {//p4
+                    n += 2;
+                    for (k = 1; k < prime.size(); k++) {//p6
+                        int quo = n / prime.at(k);
+                        int rem = n % prime.at(k);
+                        if (rem == 0) {
+                            break;
+                        }
+                        if (quo <= prime.at(k)) {
+                            check = false;
+                            break;
+                        }
+                    }
+                    if (k >= prime.size()) check = false;
+                }
+
+            }
+        }
+
     }
-    else if (schedulingMethod.compare("GUIDED_A")){
-        cout << schedulingMethod;
-    }
-    else if (schedulingMethod.compare("GUIDED_B")){
-        cout << schedulingMethod;
+    cout << "fin\n";
+    for (int i = 0; i < threads_prime.size(); i++) {
+        vector<int> tmp = threads_prime.at(i);
+        int tmp_size = tmp.size();
+        for (int z = 0; z < tmp_size; z++) {
+            primeOut.push_back(tmp.at(z));
+            cout << "" << tmp.at(z) << "\n";
+        }
     }
 
 
